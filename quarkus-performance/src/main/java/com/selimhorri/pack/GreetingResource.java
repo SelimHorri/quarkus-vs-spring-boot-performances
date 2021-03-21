10d28e7983889a9485abafafd4c3e81d0919bd11
SelimHorri <horri.slim@gmail.com>
2021-03-21 23:30:55 +0100

    add same endpoint for both applications

--- quarkus-performance/src/main/java/com/selimhorri/pack/GreetingResource.java
+++ quarkus-performance/src/main/java/com/selimhorri/pack/GreetingResource.java
@@ -5,12 +5,20 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/hello-resteasy")
+@Path("/hello")
 public class GreetingResource {
-
+	
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello RESTEasy";
+        return "Hello Quarkus !!";
     }
+    
+    
+    
 }
+
+
+
+
+
--- spring-performance/src/main/java/com/selimhorri/pack/SpringPerformanceApplication.java
+++ spring-performance/src/main/java/com/selimhorri/pack/SpringPerformanceApplication.java
@@ -2,12 +2,31 @@ package com.selimhorri.pack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping(value = {"/hello"})
 public class SpringPerformanceApplication {
-
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringPerformanceApplication.class, args);
 	}
-
+	
+	@GetMapping
+	public String hello() {
+		return "Hello Spring Boot !!";
+	}
+	
+	
+	
 }
+
+
+
+
+
+
+
