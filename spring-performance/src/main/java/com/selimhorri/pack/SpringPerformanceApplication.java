3a01660293b45f83b063be28457a384fc2c6d10e
SelimHorri <horri.slim@gmail.com>
2021-03-21 23:17:33 +0100

    init

---
+++ spring-performance/src/main/java/com/selimhorri/pack/SpringPerformanceApplication.java
@@ -0,0 +1,13 @@
+package com.selimhorri.pack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPerformanceApplication.class, args);
+	}
+
+}
